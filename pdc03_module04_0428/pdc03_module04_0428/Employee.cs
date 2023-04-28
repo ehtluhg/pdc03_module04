c81f913c13a88ed801f0d834bfd9c99cee52ff45
ehtluhg <dy.gabrielrhobert@auf.edu.ph>
2023-04-28 10:50:16 +0800

    first commit

---
+++ pdc03_module04_0428/pdc03_module04_0428/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pdc03_module04_0428
+{
+    public class Employee
+    {
+        public string DisplayName { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ pdc03_module04_0428/pdc03_module04_0428/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pdc03_module04_0428
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void activity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity01());
+        }
+
+        private async void activity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity02());
+        }
+
+    }
+}
